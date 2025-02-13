b9c67e22e55562564c3a02eabf0340f29daa38af
Florent Biville <florent.biville@neo4j.com>
2025-02-13 13:11:15 +0300

    docs: document public APIs

--- api/src/main/kotlin/org/neo4j/caniuse/Neo4jPredicate.kt
+++ api/src/main/kotlin/org/neo4j/caniuse/Neo4jPredicate.kt
@@ -2,12 +2,27 @@ package org.neo4j.caniuse
 
 import java.util.function.Predicate
 
+/**
+ * Thin wrapper type over the built-in [Predicate]. This extends predicates to be tested against a
+ * [Neo4j] descriptor. This is how feature detections are implemented.
+ */
 class Neo4jPredicate(private val predicate: Predicate<Neo4j>) {
 
+  /**
+   * Whether this feature detection passes against the provided [Neo4j] descriptor.
+   *
+   * @return [Boolean]
+   */
   fun withNeo4j(neo4j: Neo4j): Boolean {
     return predicate.test(neo4j)
   }
 
+  /**
+   * Chains this feature detection with the provided one. Both must pass for the compound detection
+   * in [withNeo4j] to return true.
+   *
+   * @return [Neo4jPredicate]
+   */
   fun and(other: Neo4jPredicate): Neo4jPredicate {
     return Neo4jPredicate(predicate.and(other.predicate))
   }
